b09db6e1be3ade97a1dff2ae03b93d12d0a820ae
RenatSayf <renatsayf@gmail.com>
2017-08-31 21:49:47 +0500

    Добавьте файлы проекта.

---
+++ ReadEnglishBooks/Controllers/BookController.cs
@@ -0,0 +1,115 @@
+using System;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ReadEnglishBooks.Models;
+using System.IO;
+using System.Collections.Generic;
+
+namespace ReadEnglishBooks.Controllers
+{
+    public class BookController : Controller
+    {
+        private BookModel book;
+        
+        // GET: Book
+        public ActionResult BookView(int page)
+        {
+            ViewBag.title = "1984. George Orwell";
+            //book = new BookModel(Directory.GetCurrentDirectory() + "\\Assets\\1984.txt", 2000);
+            //ViewBag.page0 = String.Join(String.Empty, book.PagesList.ElementAt(0).ToArray());
+            return View();
+        }
+
+        public JsonResult getPage(int page)
+        {
+            book = new BookModel(Directory.GetCurrentDirectory() + "\\Assets\\1984.txt", 4000);
+            List<string> pages = new List<string>();
+            if (page == 0)
+            {
+                pages.Add(String.Join(String.Empty, book.PagesList.ElementAt(book.PagesList.Count() - 2).ToArray()));
+                pages.Add(String.Join(String.Empty, book.PagesList.ElementAt(book.PagesList.Count() - 1).ToArray()));
+                pages.Add(String.Join(String.Empty, book.PagesList.ElementAt(page).ToArray()));
+                pages.Add(String.Join(String.Empty, book.PagesList.ElementAt(page + 1).ToArray()));
+                pages.Add(String.Join(String.Empty, book.PagesList.ElementAt(page + 2).ToArray()));
+                
+            }
+            return Json(pages);
+        }
+
+        // GET: Book/Details/5
+        public ActionResult BookDetails(int id)
+        {
+            return View();
+        }
+
+        // GET: Book/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Book/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Book/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Book/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Book/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Book/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ ReadEnglishBooks/Models/BookModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadEnglishBooks.Models
+{
+    public class BookModel
+    {
+        private IEnumerable<List<char>> pagesList;
+
+        public BookModel(string path, int pageLength)
+        {
+            if (File.Exists(path))
+            {
+                string readText = File.ReadAllText(path);
+                var charArr = readText.ToCharArray(); 
+                var pageAmount = (int)Math.Ceiling(charArr.Length / (double)pageLength);
+                pagesList = Enumerable.Range(0, pageAmount).Select(i => charArr.Skip(i * pageLength).Take(pageLength).ToList());            
+                
+
+            }            
+        }
+
+        public IEnumerable<List<char>> PagesList
+        {
+            get => pagesList;
+            set => pagesList = value;
+        }
+    }
+}
