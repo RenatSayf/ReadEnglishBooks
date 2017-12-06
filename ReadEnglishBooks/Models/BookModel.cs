746a93e6aa5adf9fb5f11615abe1aa81d86c7062
RenatSayf <renatsayf@gmail.com>
2017-12-06 22:51:23 +0500

    06.12.2017 OK

--- ReadEnglishBooks/Models/BookModel.cs
+++ ReadEnglishBooks/Models/BookModel.cs
@@ -7,6 +7,8 @@ namespace ReadEnglishBooks.Models
 {
     public class BookModel
     {
+        private string book_name;
+        private string author;
         private string[] pagesArray;
 
         public BookModel(string path)
@@ -15,14 +17,23 @@ namespace ReadEnglishBooks.Models
             {
                 string readText = File.ReadAllText(path);
                 string result = string.Join(" ", readText.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries));
-                char[] spliter = { '|' };
-                string str = readText;
-                pagesArray = result.Split(spliter);            
+                var book_array = result.Split('#');
+                pagesArray = book_array[1].Split('|');
+                var book_header_arr = book_array[0].Split('.');
+                book_name = book_header_arr[0].Trim();
+                author = book_header_arr[1].Trim();
             }
+        }
+        
+        public string BookName
+        {
+            get => book_name;
+        }
 
-
-
-        }            
+        public string Author
+        {
+            get => author;
+        }
         
         public string[] PagesArray
         {
